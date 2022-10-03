75b8ffb94e6f045f4ccbbfa5d0357187f0819f8e
maneet bhatia <maneetbhatia79@gmail.com>
2022-10-02 16:17:51 -0800

    Updated portfollio.js

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 import ProjectImg from './Assests/eventmaster.png'
-import ProjectImg1 from './Assests/eventmaster.png'
-import ProjectImg2 from './Assests/eventmaster.png'
-import ProjectImg3 from './Assests/eventmaster.png'
+import ProjectImg1 from './Assests/Critter.jpg'
+import ProjectImg2 from './Assests/ShopFit.jpg'
+import ProjectImg3 from './Assests/Space-Survival.jpg'
 import { AiFillGithub } from 'react-icons/ai';
 
 const Portfolio = () => {
@@ -27,7 +27,7 @@ const Portfolio = () => {
           <Img src={ProjectImg2} alt="Trivia Game" /></a>
           <ProjInfo>
               <h3>ShopFit</h3>
-              <a href="https://github.com/maneetbhatia/EventMaster" target="_blank">
+              <a href="https://github.com/maneetbhatia/ShopFit" target="_blank">
                 <AiFillGithub size={25}/>
               </a>
             </ProjInfo>
@@ -38,18 +38,18 @@ const Portfolio = () => {
             <Img src={ProjectImg1} alt="Rutgers Info Widget" /></a>
             <ProjInfo>
               <h3>Critter</h3>
-              <a href="https://github.com/maneetbhatia/EventMaster" target="_blank">
+              <a href="https://github.com/maneetbhatia/Critter" target="_blank">
                 <AiFillGithub size={25}/>
               </a>
             </ProjInfo>
           </Project3>
 
           <Project4>
-            <a href="https://maneet79.github.io/Guess-Game/." target="_blank"> 
+            <a href="https://maneetbhatia.github.io/space-survival/" target="_blank"> 
             <Img src={ProjectImg3} alt="Rock Paper Scissors" /></a>
             <ProjInfo>
-              <h3>GifTastic</h3>
-              <a href="https://github.com/maneetbhatia/EventMaster" target="_blank">
+              <h3>Space-Survival</h3>
+              <a href="https://github.com/maneetbhatia/Space-Survival" target="_blank">
                 <AiFillGithub size={25}/>
               </a>
             </ProjInfo>
@@ -63,57 +63,85 @@ const Portfolio = () => {
 export default Portfolio;
 
 const Main = styled.div`
-background-color: white;
+  background-color: white;
 `
 
 const Work = styled.div`
-width: 70%;
-margin: auto;
-padding-top: 80px;
-
+  width: 70%;
+  margin: auto;
+  padding-top: 80px;
 `
 
-
 const H1 = styled.h1`
-text-align: center;
-margin-bottom: 40px;
+  text-align: center;
+  margin-bottom: 40px;
 `
 
 const Wrapper = styled.div`
-display: grid;
-grid-template-columns: 45% 45%;
-grid-gap: 1%;
-justify-content: space-around;
-font-family: Georgia, Times, "Times New Roman", serif;
-padding-bottom: 50px;
+  display: grid;
+  grid-template-columns: 45% 45%;
+  grid-gap: 1%;
+  justify-content: space-around;
+  font-family: Georgia, Times, "Times New Roman", serif;
+  padding-bottom: 50px;
 `
 
 const Img = styled.img`
-width: 100%;
+  width: 100%;
+  height: 350px;
+  border-radius: 15px 15px 0px 0px;
+  border-bottom: 1px solid black;
 `
 
 const Project1 = styled.div`
-padding: 10% 0px;
-background-color: blueviolet;
+  margin: 10% 0px;
+  border-radius: 15px 15px 5px 5px;
+  box-shadow: 1px 1px 10px 1px black;
+
+  &:hover{
+    transform: scale(1.05);
+    transition: 200ms transform ease-in-out;
+  }
 
 `
 
 const Project2 = styled.div`
-padding: 10% 0px;
-background-color: blueviolet;
+  margin: 10% 0px;
+  border-radius: 15px 15px 5px 5px;
+  box-shadow: 1px 1px 10px 1px black;
+
+  &:hover{
+      transform: scale(1.05);
+      transition: 200ms transform ease-in-out;
+  }
 `
 
 const Project3 = styled.div`
-padding: 10% 0px;
-background-color: blueviolet;
+  margin: 10% 0px;
+  border-radius: 15px 15px 5px 5px;
+  box-shadow: 1px 1px 10px 1px black;
+
+  &:hover{
+    transform: scale(1.05);
+    transition: 200ms transform ease-in-out;
+  }
 `
 
 const Project4 = styled.div`
-padding: 10% 0px;
-background-color: blueviolet;
+  margin: 10% 0px;
+  box-shadow: 1px 1px 10px 1px black;
+  border-radius: 15px 15px 5px 5px;
+
+  &:hover{
+    transform: scale(1.05);
+    transition: 200ms transform ease-in-out;
+  }
 `
 
 const ProjInfo = styled.div`
-display: flex;
-justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 5px 10px 5px;
+  background-color: aliceblue;
+  border-radius: 0px 0px 5px 5px;
 `
